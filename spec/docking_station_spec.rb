6b6d5bb2fc5d229bb798f6a3f1e2fcb048edfd9f
edwardreeve <edreeve+git@gmail.com>
2019-05-16 12:06:10 +0100

    Added doubles, but tests failing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,7 @@ describe DockingStation do
   it { should respond_to :release_bike }
 
   it 'should release a working bike' do
-    bike = Bike.new
+    bike = double(:bike)
     my_dock.dock(bike)
     returned_bike = my_dock.release_bike
     expect(returned_bike.working).to eq(true)
@@ -17,7 +17,7 @@ describe DockingStation do
   it { should respond_to :bikes }
 
   it 'should store a bike that\'s been docked' do
-    bike = Bike.new
+    bike = double(:bike)
     my_dock.dock(bike)
     expect(my_dock.bikes).to eq([bike])
   end
@@ -27,8 +27,8 @@ describe DockingStation do
   end
 
   it 'should not accept bike if at capacity' do
-    DockingStation::DEFAULT_CAPACITY.times { my_dock.dock(Bike.new) }
-    expect { my_dock.dock(Bike.new) }.to raise_error('Docking station is full')
+    DockingStation::DEFAULT_CAPACITY.times { my_dock.dock(double(:bike)) }
+    expect { my_dock.dock(double(:bike)) }.to raise_error('Docking station is full')
   end
 
   it 'should accept a capacity instance variable when created' do
@@ -41,7 +41,7 @@ describe DockingStation do
   end
 
   it 'should not release broken bikes' do
-    broken_bike = Bike.new
+    broken_bike = double(:bike)
     broken_bike.broken
     my_dock.dock(broken_bike)
     expect { my_dock.release_bike }.to raise_error('Sorry, this bike is broken')
